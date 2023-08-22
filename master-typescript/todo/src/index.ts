641315181e2099905fe74d3ee0b7035e5da63c8b
augustine <augustino0890@gmail.com>
2023-08-22 15:48:40 +0900

    dom: events

--- master-typescript/todo/src/index.ts
+++ master-typescript/todo/src/index.ts
@@ -1,13 +1,16 @@
-console.log("HELLO, WORLD!");
-console.log("ADD ONE MORE THING");
-document
 const h = "hello".replaceAll("l", "i");
 console.log(h);
 
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("form")!;
 
-btn?.addEventListener("click", function () {
-    alert(input.value);
-    input.value = "";
-});
+function handleSubmit(event: SubmitEvent) {
+    event.preventDefault();
+    console.log("SUBMITTED");
+}
+form.addEventListener("submit", handleSubmit);
+// btn?.addEventListener("click", function () {
+    // alert(input.value);
+    // input.value = "";
+// });
