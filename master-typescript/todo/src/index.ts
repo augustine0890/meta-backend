e73f4afc9cbf9c2ab5b406341bf125896c55ec7a
augustine <augustino0890@gmail.com>
2023-08-22 15:38:55 +0900

    dom: type assertions

--- master-typescript/todo/src/index.ts
+++ master-typescript/todo/src/index.ts
@@ -3,3 +3,11 @@ console.log("ADD ONE MORE THING");
 document
 const h = "hello".replaceAll("l", "i");
 console.log(h);
+
+const btn = document.getElementById("btn");
+const input = document.getElementById("todoinput")! as HTMLInputElement;
+
+btn?.addEventListener("click", function () {
+    alert(input.value);
+    input.value = "";
+});
