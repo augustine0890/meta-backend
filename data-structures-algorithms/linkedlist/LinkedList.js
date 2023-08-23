6c065cbb4694281f6791d19b3374eb4f6a43ba99
augustine <augustino0890@gmail.com>
2023-08-23 18:21:56 +0900

    linked list: constructor

---
+++ data-structures-algorithms/linkedlist/LinkedList.js
@@ -0,0 +1,10 @@
+import Node from './Node.js';
+
+export class LinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = this.head;
+        this.length = 1;
+    }
+}
