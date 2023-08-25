789da29e277570d8e4f92c955f644203f864ca61
augustine <augustino0890@gmail.com>
2023-08-25 18:05:03 +0900

    linked list: push, pop, shift, unshift

--- data-structures-algorithms/index.js
+++ data-structures-algorithms/index.js
@@ -1,4 +1,19 @@
 import { LinkedList } from "./linkedlist/LinkedList.js";
 
 let myLinkedList = new LinkedList(4);
+myLinkedList.pop();
+console.log(myLinkedList);
+myLinkedList.push(7);
+myLinkedList.push(3);
+myLinkedList.push(8);
+console.log(myLinkedList);
+myLinkedList.pop();
+myLinkedList.pop();
+myLinkedList.pop();
+console.log(myLinkedList.pop());
+console.log(myLinkedList);
+myLinkedList.unshift(4);
+myLinkedList.unshift(3);
+console.log(myLinkedList);
+myLinkedList.shift();
 console.log(myLinkedList);
--- data-structures-algorithms/linkedlist/LinkedList.js
+++ data-structures-algorithms/linkedlist/LinkedList.js
@@ -7,4 +7,70 @@ export class LinkedList {
         this.tail = this.head;
         this.length = 1;
     }
+
+    // O(1)
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++
+        return this;
+    }
+
+    // O(n)
+    pop() {
+        if (!this.head) {
+            return undefined;
+        }
+
+        let temp = this.head;
+        let pre = this.head;
+        while (temp.next) {
+            pre = temp;
+            temp = temp.next;
+        }
+        this.tail = pre;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+    }
+
+    // O(n)
+    unshift(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    // O(n)
+    shift() {
+        if (!this.head) return undefined;
+        let temp = this.head;
+        this.head = temp.next;
+        temp.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.tail = null;
+        }
+        return this;
+    }
+
+    getLength() {
+        console.log("Length: " + this.length);
+    }
 }
