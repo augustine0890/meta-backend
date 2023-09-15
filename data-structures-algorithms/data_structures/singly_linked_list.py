31f8191b3d5fd28a956e2a50b8bd365ae31cef0b
augustine <augustino0890@gmail.com>
2023-09-15 17:54:39 +0900

    singly linked list: append

--- data-structures-algorithms/data_structures/singly_linked_list.py
+++ data-structures-algorithms/data_structures/singly_linked_list.py
@@ -9,3 +9,22 @@ class SinglyLinkedList:
         self.head = None
         self.tail = None
         self._length = 0
+
+    # Time: O(1) and Space: O(1)
+    def append(self, value):
+        new_node = Node(value)
+        if not self._length:
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self._length += 1
+        return self
+
+
+my_list = SinglyLinkedList()
+print(my_list._length)
+print(my_list.append(2))
+print(my_list._length)
+print(my_list.head.value)
+print(my_list.tail.value)
