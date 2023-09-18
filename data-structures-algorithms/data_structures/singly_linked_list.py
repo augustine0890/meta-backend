d2e2f3bd5e16ad90c0fc36ce9a79f6c271988c6c
augustine <augustino0890@gmail.com>
2023-09-18 12:53:36 +0900

    sll: pop left

--- data-structures-algorithms/data_structures/singly_linked_list.py
+++ data-structures-algorithms/data_structures/singly_linked_list.py
@@ -32,10 +32,28 @@ class SinglyLinkedList:
         self._length += 1
         return self
 
+    # Time: O(1) and Space: O(1)
+    def pop_left(self):
+        if not self._length:
+            raise Exception("list is empty")
+
+        removed_value = self.head.value
+
+        if self._length == 1:
+            self.head = self.tail = None
+        else:
+            temp = self.head
+            self.head = self.head.next
+            temp.next = None
+        self._length -= 1
+        return removed_value
+
 
 my_list = SinglyLinkedList()
 print(my_list._length)
 print(my_list.append(2))
-print(my_list._length)
-print(my_list.head.value)
-print(my_list.tail.value)
+print("Length", my_list._length)
+print("Head", my_list.head.value)
+print("Tail", my_list.tail.value)
+my_list.prepend(11)
+print("Head after prepend", my_list.head.value)
