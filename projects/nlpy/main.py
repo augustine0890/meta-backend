20db856f93d3b12f271e9cd567379f364b0a3788
augustine <augustino0890@gmail.com>
2023-09-20 17:24:10 +0900

    project: nlp package for python

--- projects/nlpy/main.py
+++ projects/nlpy/main.py
@@ -1,9 +1,41 @@
-import re
+from http import HTTPStatus
+
+from flask import Flask, jsonify, request
+import nlp
+
+app = Flask(__name__)
 
 
-def stem(word):
-    return re.sub(r'(s|ing)$', '', word)
+def json_error(error, status_code=HTTPStatus.BAD_REQUEST):
+    resp = jsonify(error=error)
+    resp.status_code = status_code
+    return resp
 
 
-print(stem('working'))
-print(stem('works'))
+@app.route('/tokenize', methods=['POST'])
+def tokenize():
+    data = request.get_data()
+    if not data:
+        return json_error('empty request')
+    try:
+        text = data.decode('utf-8')
+    except UnicodeDecodeError as err:
+        return json_error(str(err))
+
+    return jsonify(tokens=nlp.tokenize(text))
+
+
+@app.route('/health')
+def health():
+    return jsonify(health=True)
+
+
+def main():
+    from os import environ
+
+    port = int(environ.get('NLP_PORT', '8080'))
+    app.run(debug='DEBUG' in environ, port=port)
+
+
+if __name__ == '__main__':
+    main()
