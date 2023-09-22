b9ef2b6fefc3959246094f2dcfb5c4b716668487
augustine <augustino0890@gmail.com>
2023-09-22 17:27:37 +0900

    express: create basic data endpoint

--- projects/essentials/index.js
+++ projects/essentials/index.js
@@ -5,7 +5,22 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.json(data);
+});
+
+app.post('/create', (req, res) => {
+    res.send("This is a POST request at /create")
+});
+
+app.put('/edit', (req, res) => {
+    res.send("This is a PUT request at /edit")
+});
+
+app.delete('/delete', (req, res) => {
+    res.send("This is a DELETE request at /delete")
+})
+
 app.listen(PORT, () => {
     console.log(`The server is running at ${PORT}`);
-    console.log(data);
 });
