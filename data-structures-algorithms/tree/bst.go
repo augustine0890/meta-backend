3b5f5f04a179b7dd9a31972f721209a59958ad8a
augustine <augustino0890@gmail.com>
2023-11-03 16:37:36 +0900

    contains value in tree

--- data-structures-algorithms/tree/bst.go
+++ data-structures-algorithms/tree/bst.go
@@ -19,20 +19,65 @@ func NewBinaryTree() *BinaryTree {
 	return &BinaryTree{}
 }
 
+// func (bt *BinaryTree) Insert(value int) {
+// bt.Root = insert(bt.Root, value)
+// }
+
+// func insert(node *Node, value int) *Node {
+// if node == nil {
+// return &Node{Value: value}
+// }
+// if value < node.Value {
+// node.Left = insert(node.Left, value)
+// } else if value > node.Value {
+// node.Right = insert(node.Right, value)
+// }
+// return node
+// }
+
+// Space: O(1), Time: O(logN); Time Worst: O(N)
 func (bt *BinaryTree) Insert(value int) {
-	bt.Root = insert(bt.Root, value)
+	if bt.Root == nil {
+		bt.Root = &Node{Value: value}
+		return
+	}
+
+	currentNode := bt.Root
+	for {
+		if value < currentNode.Value {
+			if currentNode.Left == nil {
+				currentNode.Left = &Node{Value: value}
+				break
+			} else {
+				currentNode = currentNode.Left
+			}
+		} else if value > currentNode.Value {
+			if currentNode.Right == nil {
+				currentNode.Right = &Node{Value: value}
+				break
+			} else {
+				currentNode = currentNode.Right
+			}
+		} else {
+			// Value already exist in the tree; do not insert duplicates.
+			break
+		}
+	}
 }
 
-func insert(node *Node, value int) *Node {
-	if node == nil {
-		return &Node{Value: value}
+// Space: O(N); Time Best: O(logN); Time Worst: O(N)
+func (bt *BinaryTree) Contains(value int) bool {
+	currentNode := bt.Root
+	for currentNode != nil {
+		if value == currentNode.Value {
+			return true
+		} else if value > currentNode.Value {
+			currentNode = currentNode.Right
+		} else {
+			currentNode = currentNode.Left
+		}
 	}
-	if value < node.Value {
-		node.Left = insert(node.Left, value)
-	} else if value > node.Value {
-		node.Right = insert(node.Right, value)
-	}
-	return node
+	return false
 }
 
 // Returns a new, random binary search tree holding the values 1k, 2k, ..., nk.
@@ -48,5 +93,6 @@ func main() {
 	tree := CreateRandomBinaryTree(10, 2)
 	fmt.Println(tree)
 	tree.Insert(110)
-	fmt.Println(tree)
+	fmt.Println(tree.Contains(20)) // true
+	fmt.Println(tree.Contains(11)) // false
 }
