3365d9167cad53468ed070ad73d938ba167b6c21
augustine <augustino0890@gmail.com>
2023-11-21 16:50:18 +0900

    exercise: swapping two nodes in linked-list

--- data-structures-algorithms/linkedlist/linked_list.py
+++ data-structures-algorithms/linkedlist/linked_list.py
@@ -30,8 +30,9 @@ class LinkedList:
         string_list = ""
         current_node = self.get_head_node()
         while current_node:
-            if current_node.get_value() is not None:
-                string_list += str(current_node.get_value()) + "\n"
+            string_list += str(current_node.get_value())
+            if current_node.get_next_node() is not None:
+                string_list += " --> "
             current_node = current_node.get_next_node()
         return string_list
 
@@ -47,9 +48,61 @@ class LinkedList:
                 return
             previous_node, current_node = current_node, current_node.get_next_node()
 
+    def swap(self, val1, val2):
+        if val1 == val2:
+            print("Elements are the same - no swap needed")
+            return
+
+        node1 = self.head_node
+        node2 = self.head_node
+        node1_prev = None
+        node2_prev = None
+
+        # Find nodes with val1 and val2
+        while node1 and node1.get_value() != val1:
+            node1_prev = node1
+            node1 = node1.get_next_node()
+
+        while node2 and node2.get_value() != val2:
+            node2_prev = node2
+            node2 = node2.get_next_node()
+
+        # Check if both elements are in the list
+        if not node1 or not node2:
+            print("Swap not possible - one or more element is not in the list")
+            return
+
+        # Swapping the head node if needed
+        if node1_prev is None:
+            self.head_node = node2
+        else:
+            node1_prev.set_next_node(node2)
+
+        if node2_prev is None:
+            self.head_node = node1
+        else:
+            node2_prev.set_next_node(node1)
+
+        # Swapping the next nodes of node1 and node2
+        temp = node1.get_next_node()
+        node1.set_next_node(node2.get_next_node())
+        node2.set_next_node(temp)
+
 
 ll = LinkedList(5)
 ll.insert_beginning(70)
 ll.insert_beginning(5675)
 ll.insert_beginning(90)
+print(ll.stringify_list())
+
+ll = LinkedList()
+for i in range(10):
+    ll.insert_beginning(i)
+
+print(ll.stringify_list())
+ll.swap(9, 8)
+print(ll.stringify_list())
+ll.swap(5, 4)
+print(ll.stringify_list())
+ll.swap(1, 0)
 print(ll.stringify_list())
