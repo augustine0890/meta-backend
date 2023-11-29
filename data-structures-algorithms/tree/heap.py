abdfb8d053d8dfb124e3499b1a460ab12c3bd2a1
augustine <augustino0890@gmail.com>
2023-11-29 16:23:19 +0900

    Kth smallest element in an array

--- data-structures-algorithms/tree/heap.py
+++ data-structures-algorithms/tree/heap.py
@@ -91,3 +91,34 @@ class MinHeap:
         self.heap[0] = self.heap.pop()
         self._sink_down(0)
         return min_value
+
+
+# Time: O(Nlogk), Space: O(K)
+def find_kth_smallest(nums, k):
+    max_heap = MaxHeap()
+
+    for num in nums:
+        max_heap.insert(num)
+        if len(max_heap.heap) > k:
+            max_heap.remove()
+    return max_heap.remove()
+
+
+# Test cases
+nums = [
+    [3, 2, 1, 5, 6, 4],
+    [6, 5, 4, 3, 2, 1],
+    [1, 2, 3, 4, 5, 6],
+    [3, 2, 3, 1, 2, 4, 5, 5, 6],
+]
+ks = [2, 3, 4, 7]
+expected_outputs = [2, 3, 4, 5]
+
+for i in range(len(nums)):
+    print(f"Test case {i+1}...")
+    print(f"Input: {nums[i]} with k = {ks[i]}")
+    result = find_kth_smallest(nums[i], ks[i])
+    print(f"Output: {result}")
+    print(f"Expected output: {expected_outputs[i]}")
+    print(f"Test passed: {result == expected_outputs[i]}")
+    print("---------------------------------------")
