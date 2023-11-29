fc26939e348b1216e4663a82d665a82a553a5ac9
augustine <augustino0890@gmail.com>
2023-11-29 15:58:50 +0900

    ds: MinHeap

--- data-structures-algorithms/tree/heap.py
+++ data-structures-algorithms/tree/heap.py
@@ -9,7 +9,7 @@ class MaxHeap:
         return 2 * index + 2
 
     def _parent(self, index):
-        return (index + 1) // 2
+        return (index - 1) // 2
 
     def _swap(self, index1, index2):
         self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
@@ -23,6 +23,7 @@ class MaxHeap:
             self._swap(current, self._parent(current))
             current = self._parent(current)
 
+    # Time O(logN)
     def _sink_down(self, index):
         max_index = index
         while True:
@@ -55,3 +56,38 @@ class MaxHeap:
         self.heap[0] = self.heap.pop()
         self._sink_down(0)
         return max_value
+
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index):
+        return 2 * index + 1
+
+    def _right_child(self, index):
+        return 2 * index + 2
+
+    def _parent_child(self, index):
+        return (index - 1) // 2
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+        if current > 0 and self.heap[current] < self.heap[self._parent_child(current)]:
+            self._swap(current, self._parent_child(current))
+            current = self._parent_child(current)
+
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        min_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+        return min_value
