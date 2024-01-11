ea015ae5e8667dbc9a7281cefd8f8c078ba50ed5
augustine <augustino0890@gmail.com>
2024-01-11 15:42:12 +0900

    python project: test with multiple parameters

--- projects/template/tests/test_reminder.py
+++ projects/template/tests/test_reminder.py
@@ -1,13 +1,13 @@
+import pytest
 import src.reminder as app
 from src.reminder import Task
 
 
-def test_find_task():
+@pytest.mark.parametrize("test_input, expected",
+                         [("buy usdt", Task(name="buy usdt")),
+                          ("buy solana", None),
+                          ("PAY RENT", Task(name="pay rent"))
+                          ])
+def test_find_task(test_input, expected):
     task_list = [Task(name="pay rent"), Task(name="buy usdt")]
-    assert app._find_task("buy usdt", task_list) == Task(name="buy usdt")
-    assert app._find_task("buy usdt", task_list) != Task(name="pay rent")
-
-
-def test_none_find_task():
-    task_list = [Task(name="pay rent"), Task(name="buy usdt")]
-    assert app._find_task("buy btc", task_list) == None
+    assert app._find_task(test_input, task_list) == expected
